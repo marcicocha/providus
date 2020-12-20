abde82bdf5a94c852cfa7342ac312849d72525bf
unknown <marcia.cole@longbridgetech.com>
2020-12-20 23:01:59 +0100

    feat: linter fixes

---
+++ components/UI/AppTitleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ heading }}</h1>
+    <p v-if="description">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitleComponent',
+  props: {
+    heading: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AppTitleComponent
+      heading="Account Creation Web Portal"
+      :description="description"
+    />
+  </div>
+</template>
+
+<script>
+import AppTitleComponent from '@/components/UI/AppTitleComponent'
+export default {
+  components: {
+    AppTitleComponent,
+  },
+  data() {
+    return {
+      description:
+        'Welcome to Future Forward Banking. We believe that the New World of fast, smart, personal and borderless banking relationship is here. Now let’s guide you through the process of creating your very own Future Forward Bank account.',
+    }
+  },
+}
+</script>
