d7b4105aebe073684b3ac4850c905f862a0a3473
dafom <ayomidechollom@gmail.com>
2020-12-22 09:12:33 +0100

    feat: admin dashboard layout setup

---
+++ components/AppAdminSideBar.vue
@@ -0,0 +1,97 @@
+<template>
+  <aside class="menu">
+    <!-- <p class="menu-label">General</p> -->
+    <ul class="menu-list">
+      <p class="menu-label">
+        <img src="../assets/images/logowhitetext.svg" alt="" />
+      </p>
+    </ul>
+    <ul class="menu-list">
+      <li v-for="(menu, index) in menuList" :key="index">
+        <a :href="menu.path">{{ menu.name }}</a>
+      </li>
+    </ul>
+    <ul class="menu-list log-out">
+      <li><a>Logout</a></li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    menuList() {
+      const menu = [
+        {
+          name: 'User Management',
+          path: '/admin/dashboard/usermanagement',
+          children: [],
+        },
+        {
+          name: 'Settings',
+          path: '/admin/dashboard/setting',
+          children: [],
+        },
+      ]
+      return menu
+    },
+  },
+  watch: {},
+  methods: {},
+}
+</script>
+<style lang="scss" scoped>
+.menu {
+  background: #2e434e;
+  height: 100vh;
+  position: fixed;
+  color: #fff;
+  border-right: 5px solid #fdb813;
+  box-shadow: 1px 0.1px 15px -6px rgba(51, 51, 51, 1);
+  -webkit-box-shadow: 1px 0.1px 15px -6px rgba(51, 51, 51, 1);
+  -moz-box-shadow: 1px 0.1px 15px -6px rgba(51, 51, 51, 1);
+  .menu-list .menu-label {
+    // text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #f8f8f82c;
+    margin-bottom: 25px;
+    img {
+      width: 150px;
+      height: 150px;
+      margin: 5px 1px;
+    }
+  }
+  .menu-list li a {
+    padding: 10px 30px;
+    font-family: 'GothamLight', sans-serif;
+    font-style: normal;
+    font-size: 20px;
+    line-height: 150%;
+    color: #fff;
+    transition: all 0.3s;
+    &:hover {
+      color: #2e434e;
+      background: #fdb813;
+      font-weight: bold;
+    }
+    &:focus {
+      color: red;
+      background: #fdb813;
+    }
+  }
+  .log-out {
+    position: absolute;
+    bottom: 10%;
+    width: 100%;
+  }
+}
+</style>
---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>hi moffo</div>
+</template>
+
+<script>
+// import AppTitleComponent from '@/components/UI/AppTitleComponent'
+// import AppInput from '@/components/UI/AppInput.vue'
+// import AppButton from '@/components/UI/AppButton.vue'
+export default {
+  layout: 'dashboard',
+  components: {
+    // AppTitleComponent,
+    // AppInput,
+    // AppButton,
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
