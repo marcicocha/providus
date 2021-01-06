55c153f81ec8c2249ef68271030adea2aef24e5f
unknown <marcia.cole@longbridgetech.com>
2021-01-06 15:32:42 +0100

    feat: added button the index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,23 @@
       heading="Account Creation Web Portal"
       :description="description"
     />
+
+    <AppButton title="GET STARTED" />
+    <p>
+      Have a Reference Number?<a
+        ><nuxt-link to="/user/AppReference">Continue</nuxt-link></a
+      >
+    </p>
   </div>
 </template>
 
 <script>
 import AppTitleComponent from '@/components/UI/AppTitleComponent'
+import AppButton from '@/components/UI/AppButton'
 export default {
   components: {
     AppTitleComponent,
+    AppButton,
   },
   data() {
     return {
