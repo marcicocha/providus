9e2e8b6de31192ca9ab0b175ec39c3885318e79c
Dika Abuka <buzz@dabuka.com.ng>
2021-01-16 16:51:30 +0100

    fix(server): loader component / shell injected

---
+++ components/UI/AppLoader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="loader">
+    <img class="loader-img" src="../../assets/images/logo.svg" alt="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLoader',
+}
+</script>
+
+<style scoped>
+.loader {
+  display: table;
+  background: #fff;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden !important;
+  z-index: 999999999999999;
+}
+.loader-img {
+  margin: 42vh auto auto auto;
+  display: table-cell;
+  vertical-align: middle;
+  position: relative;
+  width: 155px;
+  height: auto;
+}
+</style>
