b2b9d16746255bb6f3c3cac4a076adbd4c0d6a14
unknown <marcia.cole@longbridgetech.com>
2021-02-05 10:30:06 +0100

    fix: bak button fix on capture page

--- pages/user/individual/capture-id.vue
+++ pages/user/individual/capture-id.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <a class="back-button" @click="backButtonHandler"
+      ><img src="~assets/images/back-arrow.svg" alt="back-button" />
+      <span>Back</span></a
+    >
     <AppTitleComponent
       heading="Capture ID"
       description="Place ID within the edges of the rectangle."
@@ -16,5 +20,30 @@ export default {
     AppTitleComponent,
     AppCaptureId,
   },
+  methods: {
+    backButtonHandler() {
+      this.$router.replace('/user/individual/upload-valid-id')
+    },
+  },
 }
 </script>
+<style lang="scss" scoped>
+.back-button {
+  span {
+    margin-left: 5px;
+  }
+}
+
+@media only screen and (min-width: 1536px) {
+  @media only screen and (min-height: 730px) {
+    .back-button {
+      font-size: 13px !important;
+    }
+  }
+  @media only screen and (min-height: 900px) {
+    .back-button {
+      font-size: 1em;
+    }
+  }
+}
+</style>
