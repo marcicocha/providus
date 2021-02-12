aefd5a037e0db25b006be60783747118311ecc8c
Falua Temitope <ayomidechollom@gmail.com>
2021-02-12 23:17:06 +0100

    feat: changes

--- components/user/individual/capture/AppCaptureSelfie.vue
+++ components/user/individual/capture/AppCaptureSelfie.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-if="!loading">
-    <i class="fa fa-download" @click="pdfMethod">Download</i>
     <div class="container">
       <video
         v-show="!selfieCapture"
@@ -72,7 +71,6 @@
 </template>
 <script>
 import AppButton from '@/components/UI/AppButton'
-import { jsPDF as PdfClass } from 'jspdf'
 export default {
   name: 'AppCaptureSelfie',
   components: {
@@ -133,12 +131,6 @@ export default {
       this.imgSrc = ''
       this.selfieCapture = false
     },
-    pdfMethod(data) {
-      console.log(data, '::: pdf data :::')
-      const doc = new PdfClass()
-      doc.text(data.toString(), 10, 10)
-      doc.save('a4.pdf')
-    },
     async nextHandler() {
       try {
         const file = new File([this.imgSrc], 'selfie.jpg', {
@@ -150,7 +142,6 @@ export default {
         formData.append('file', file)
         formData.append('requestId', requestId)
         document.querySelector('#stop-capture').click()
-        this.pdfMethod(formData)
         await this.$axios.$post('/individual/selfieUpload', formData)
         this.$router.replace('/user/individual/upload-valid-id')
       } catch (err) {
