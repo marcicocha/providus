6d54cefe68e260f3671c958e71c766111cc57784
unknown <marcia.cole@longbridgetech.com>
2021-03-09 18:13:45 +0100

    fix: canvas fixes

--- static/daon/doc/app.js
+++ static/daon/doc/app.js
@@ -9,8 +9,9 @@ let queryParams = {}
 let videoNew = null
 let canvas = document.querySelector('canvas')
 let doc_type = 'PASSPORT'
-let overlayWidth = 500
-let ID_CARD_RATIO = 1.5858
+let overlayWidth = 550
+// let ID_CARD_RATIO = 1.5858
+let ID_CARD_RATIO = 1.2858
 let PASSPORT_RATIO = 1.4205
 
 let dc = new Daon.DocumentCapture({
